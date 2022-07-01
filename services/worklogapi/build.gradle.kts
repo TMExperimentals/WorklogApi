8de92a91e839340c04a1d70aa5d9fefa9c94ece5
Bibek Saini <itsbibeksaini@gmail.com>
2022-07-01 22:44:21 +0530

    chore: applying editorconfig

--- services/worklogapi/build.gradle.kts
+++ services/worklogapi/build.gradle.kts
@@ -2,11 +2,11 @@ val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 
-plugins{
+plugins {
     kotlin("jvm") version "1.7.0"
 }
 
-dependencies{
+dependencies {
     implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
--- services/worklogapi/src/main/kotlin/com/tmsolution/worklogapi/Application.kt
+++ services/worklogapi/src/main/kotlin/com/tmsolution/worklogapi/Application.kt
@@ -3,8 +3,8 @@ package com.tmsolution.worklogapi
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
-fun main(){
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0"){
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
     }.start(wait = true)
 }
