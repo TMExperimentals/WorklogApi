c2ad94fbf2d30e18543895a950066d3a88b5b226
Bibek Saini <itsbibeksaini@gmail.com>
2022-07-05 18:23:53 +0530

    chore: setting up  Koin-ktor DI

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,15 @@ subprojects {
         dependencyManagement {
             imports {
                 mavenBom("io.ktor:ktor-bom:2.0.3")
+                mavenBom("org.jetbrains.exposed:exposed-bom:0.38.2")
+                mavenBom("org.jetbrains.kotlin:kotlin-bom:1.7.0")
             }
 
             dependencies {
                 dependency("ch.qos.logback:logback-classic:1.2.11")
+                dependencySet("io.insert-koin:3.2.0"){
+                    entry("koin-ktor")
+                    entry("koin-logger-slf4j")
             }
         }
     }
--- services/worklogapi/build.gradle.kts
+++ services/worklogapi/build.gradle.kts
@@ -8,6 +8,8 @@ dependencies {
     implementation("ch.qos.logback:logback-classic")
     implementation("io.ktor:ktor-server-auth")
     implementation("io.ktor:ktor-server-auth-jwt")
+    implementation("io.insert-koin:koin-ktor")
+    implementation("io.insert-koin:koin-logger-slf4j")
 
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
