e10085749fc67543a73f4fa2ef4ed87294f93d2e
Bibek Saini <itsbibeksaini@gmail.com>
2022-07-06 00:08:11 +0530

    chore: adding hikariCP config
    - setting up DI with koin
    - DB config with hikariCP

---
+++ services/worklogapi/src/main/kotlin/com/tmsolution/worklogapi/core/WorklogStoreFactory.kt
@@ -0,0 +1,35 @@
+package com.tmsolution.worklogapi.core
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.config.*
+import io.ktor.server.engine.*
+import org.jetbrains.exposed.sql.Database
+
+class WorklogStoreFactory(
+    private val environment: ApplicationConfig
+) {
+
+    fun init(){
+        Database.connect(hikariInit())
+    }
+
+    private fun hikariInit(): HikariDataSource{
+
+
+        val dbConfig = this.environment.config("ktor.database")
+        val config = HikariConfig().apply {
+            jdbcUrl = dbConfig.property("connection.jdbc").getString()
+//            driverClassName = dbConfig.property("connection.driverClassName").getString()
+            username = dbConfig.property("connection.user").getString()
+            password = dbConfig.property("connection.password").getString()
+            isAutoCommit = false
+            println(dbConfig.property("connection.maximumPoolSize").getString())
+            maximumPoolSize = dbConfig.property("connection.maximumPoolSize").getString().toInt()
+            transactionIsolation = dbConfig.property("connection.transactionIsolation").getString()
+            validate()
+        }
+
+        return HikariDataSource(config)
+    }
+}
--- services/worklogapi/src/main/kotlin/com/tmsolution/worklogapi/plugins/Modules.kt
+++ services/worklogapi/src/main/kotlin/com/tmsolution/worklogapi/plugins/Modules.kt
@@ -1,17 +1,36 @@
 package com.tmsolution.worklogapi.plugins
 
+import com.tmsolution.worklogapi.core.WorklogStoreFactory
+import com.tmsolution.worklogapi.core.repositories.WorklogsRepository
+import com.tmsolution.worklogapi.core.repositories.WorklogsRepositoryImpl
+import com.typesafe.config.ConfigFactory
 import io.ktor.server.application.*
+import io.ktor.server.config.*
+import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.module
+import org.koin.fileProperties
+import org.koin.ktor.ext.inject
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
+
+val worklogModule = module {
+    singleOf<WorklogStoreFactory> { WorklogStoreFactory(HoconApplicationConfig(ConfigFactory.load())) }
+    singleOf<WorklogsRepository> { WorklogsRepositoryImpl() }
+
+}
+
 fun Application.modules(){
+
     configureRouting()
     jwtAuthentication()
 
     install(Koin){
         slf4jLogger()
+        modules(worklogModule)
     }
 
-
+    val store: WorklogStoreFactory by inject()
+    store.init()
 
 }
