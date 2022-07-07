f091cee3279730b398ee314296e7d3f3fa4c3cb9
Bibek Saini <itsbibeksaini@gmail.com>
2022-07-07 23:28:38 +0530

    chore: updating dependencies

--- services/worklogapi/build.gradle.kts
+++ services/worklogapi/build.gradle.kts
@@ -3,14 +3,13 @@ plugins {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core")
-    implementation("io.ktor:ktor-server-netty")
-    implementation("io.ktor:ktor-server-auth")
-    implementation("io.ktor:ktor-server-auth-jwt")
-    implementation("io.ktor:ktor-server-locations")
-
-//    implementation("io.ktor:ktor-jackson")
-    implementation("io.ktor:ktor-server-content-negotiation")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-netty-jvm")
+    implementation("io.ktor:ktor-server-auth-jvm")
+    implementation("io.ktor:ktor-server-auth-jwt-jvm")
+    implementation("io.ktor:ktor-server-locations-jvm")
+    implementation("io.ktor:ktor-serialization-jackson-jvm")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm")
 
     implementation("io.insert-koin:koin-ktor")
     implementation("io.insert-koin:koin-logger-slf4j")
@@ -26,7 +25,7 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-sqlserver")
 
-    testImplementation("io.ktor:ktor-server-test-host")
+    testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("com.h2database:h2")
 
